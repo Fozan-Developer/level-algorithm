64bdd27369aa887eeb82153ae5f6fa67f6a41d1b
ФОЗАН <nazar.korkin@mail.ru>
2024-07-04 13:41:37 +0300

    update

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-const calculateExperience = require("./lib/calculateExperience");
-const calculateLevel = require('./lib/calculateLevel');
-const calculateExperienceRemaining = require('./lib/calculateExperienceRemaining');
-const calculateExperienceProgress = require('./lib/calculateExperienceProgress');
+const {
+    calculateExperience,
+    calculateLevel,
+    calculateExperienceRemaining,
+    calculateExperienceProgress
+} = require('./lib');
 
 class LevelAlgorithm {
     constructor({ baseExperience, experienceMultiplier }) {
-        this.baseExperience = baseExperience,
-        this.experienceMultiplier = experienceMultiplier
-    };
+        this.baseExperience = baseExperience;
+        this.experienceMultiplier = experienceMultiplier;
+    }
 
     calculateExperience(level) {
-        return calculateExperience({ level: level, baseExperience: this.baseExperience, experienceMultiplier: this.experienceMultiplier });
-    };
+        return calculateExperience({ level, baseExperience: this.baseExperience, experienceMultiplier: this.experienceMultiplier });
+    }
     
     calculateLevel(experience) {
-        return calculateLevel({ experience: experience, baseExperience: this.baseExperience, experienceMultiplier: this.experienceMultiplier });
-    };
+        return calculateLevel({ experience, baseExperience: this.baseExperience, experienceMultiplier: this.experienceMultiplier });
+    }
 
     calculateExperienceRemaining(level, experience) {
-        return calculateExperienceRemaining({ level: level, experience: experience, baseExperience: this.baseExperience, experienceMultiplier: this.experienceMultiplier });
-    };
+        return calculateExperienceRemaining({ level, experience, baseExperience: this.baseExperience, experienceMultiplier: this.experienceMultiplier });
+    }
 
     calculateExperienceProgress(level, experience) {
-        return calculateExperienceProgress({ level: level, experience: experience, baseExperience: this.baseExperience, experienceMultiplier: this.experienceMultiplier });
-    };
-};
+        return calculateExperienceProgress({ level, experience, baseExperience: this.baseExperience, experienceMultiplier: this.experienceMultiplier });
+    }
+}
 
 module.exports = LevelAlgorithm;
